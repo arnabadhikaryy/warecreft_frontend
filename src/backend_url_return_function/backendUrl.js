6bebf47d42a4f5f0bcee71117f662c7cbb0c9739
Goutam Mandal <goutammandal@Goutams-MacBook-Air.local>
2026-06-20 17:32:40 +0530

    suscribe the railway

--- src/backend_url_return_function/backendUrl.js
+++ src/backend_url_return_function/backendUrl.js
@@ -1,5 +1,6 @@
 //const backend_Url = 'http://localhost:3000';
-const backend_Url = 'https://warcraft.onrender.com';
+//const backend_Url = 'https://warcraft.onrender.com';
+const backend_Url = 'https://warcraft-production-f9a0.up.railway.app';
 const adminphone = '7365075168';
 //const payment_mode = 'sandbox';
 const payment_mode = 'production';
